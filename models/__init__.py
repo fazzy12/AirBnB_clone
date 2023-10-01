a4bed666554f2361bdda486078d5e49fbc249f03
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-01 20:37:04 +0100

    Implement FileStorage class for serializing instances to JSON and deserializing JSON to instances.

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python3
-"""
-__init__ dunder method for the models directory
-"""
-from models.engine.file_storage import FileStorage
+#!/usr/bin/env python3
+"""__init__ dunder method for the models directory"""
 
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
