420bab064fa09188590cf74b3cf08a3aa8ca5e6e
fazzy12 <kaluifeanyi00@gmail.com>
2023-10-09 16:09:54 +0100

    Update console.py for additional classes and actions

--- console.py
+++ console.py
@@ -100,22 +100,22 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, arg):
         """Prints all string representation of all instances"""
         args = arg.split()
-        if not args:
-            print("** class name missing **")
-            return
-        class_name = args[0]
-        if class_name not in {"BaseModel",
-                              "User",
-                              "State",
-                              "City",
-                              "Amenity",
-                              "Place",
-                              "Review"}:
+        if args and args[0] not in {"BaseModel",
+                                    "User",
+                                    "State",
+                                    "City",
+                                    "Amenity",
+                                    "Place",
+                                    "Review"}:
             print("** class doesn't exist **")
             return
 
-        instances = eval(f"{class_name}.all()")
-        print([str(instance) for instance in instances])
+        if not args:
+            print([str(value) for value in storage.all().values()])
+        else:
+            class_name = args[0]
+            print([str(value) for key, value in storage.all().items()
+                   if key.startswith(class_name + ".")])
 
     def do_update(self, arg):
         """Updates an instance based on the class name and id"""
